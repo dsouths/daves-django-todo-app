a9d9a6f10f911704d6e32451d7d75aefb631d24a
dsouths <dsouths@hotmail.com>
2023-03-09 21:37:49 +0000

    got initial hello to appear

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render,
-
-HttpResponse
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
 def say_hello(request):
     return HttpResponse("Hello!")
-
