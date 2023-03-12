a229f5eb9189f0997918dbab3243470361ea907c
dsouths <dsouths@hotmail.com>
2023-03-12 12:25:52 +0000

    created CRUD functionality

--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404   
 from .models import Item
 from .forms import ItemForm
-
-
 
 
 def get_todo_list(request):
@@ -24,3 +22,33 @@ def add_item(request):
         'form': form
     }  
     return render(request, 'todo/add_item.html', context)
+
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save('get_todo_list')
+
+        return redirect('get_todo_list')
+    form = ItemForm()
+    context = {
+        'form': form
+    }
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }  
+
+    return render(request, 'todo/edit_item.html', context)
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
