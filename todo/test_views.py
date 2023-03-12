3a4071fe35ff4891695142fbeccf0acbb65e272b
dsouths <dsouths@hotmail.com>
2023-03-12 13:52:17 +0000

    adding testing for CRUD functionality

---
+++ todo/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from .models import Item
+
+# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_get_todo_list(self):
+        reponse = self.client.get('/')
+        self.assertEqual(reponse.status_code, 200)
+        self.assertTemplateUsed(reponse, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        reponse = self.client.get('/add')
+        self.assertEqual(reponse.status_code, 200)
+        self.assertTemplateUsed(reponse, 'todo/add_item.html')
+    
+    def test_get_edit_item_page(self):
+        item = Item.objects.create(name='Test Todo Item')
+        reponse = self.client.get(f'/edit/{item.id}')
+        self.assertEqual(reponse.status_code, 200)
+        self.assertTemplateUsed(reponse, 'todo/edit_item.html')
+
+    # def test_can_add_item(self):
+    
+    # def test_can_delete_item(self):
+    
+    # def test_can_toggle_item(self):
