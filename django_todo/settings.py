bf6b236a0ad5b7fd2179a00807c741646e3c9cbe
dsouths <dsouths@hotmail.com>
2023-03-20 11:47:42 +0000

    debug = true

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 import os
 import env
 
+development = os.environ.get('DEVELOPMENT', False)
+
 if os.path.isfile("env.py"):
     import env  
 
@@ -28,10 +30,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gx99r3-9p__*6g+evsznz7k(@ohmf1r^i4ot3e03%(&s=u=-xu'
+SECRET_KEY = os.environ.get('SECRET_KEY','')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
 ALLOWED_HOSTS = []
 
